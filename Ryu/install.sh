7cfbb307677c6a17de7fcf8f8d032b8a7e690c99
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-07-30 09:46:21 +0800

    update

--- Ryu/install.sh
+++ Ryu/install.sh
@@ -1,6 +1,7 @@
 apt update
 apt install python-pip
-pip install grpcio -i https://mirrors.aliyun.com/pypi/simple/ 
+apt install python3-pip
+pip install grpcio -i https://mirrors.aliyun.com/pypi/simple/
 pip install grpcio-tools -i https://mirrors.aliyun.com/pypi/simple/ 
 apt install mysql-client-core-5.7
 apt install mysql-client-5.7
@@ -10,4 +11,4 @@ apt install libmysqlclient-dev
 pip install MySQL-python -i https://mirrors.aliyun.com/pypi/simple/
 pip install sqlalchemy -i https://mirrors.aliyun.com/pypi/simple/
 pip install flask
-pip install pyecharts
+pip3 install pyecharts
