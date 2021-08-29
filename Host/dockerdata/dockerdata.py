5289b7859dbbef14c367c482e17bc89a844b68f4
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-08-29 10:02:32 +0800

    update

--- Host/dockerdata/dockerdata.py
+++ Host/dockerdata/dockerdata.py
@@ -4,7 +4,12 @@ import docker
 cli = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 def getDockerData():
-    return cli.containers()
+    datalist = cli.containers()
+    retData = []
+    for i in cli.containers():
+        i["stats"] = cli.stats(i["Id"], stream=False)
+        retData.append(i)
+    return retData
 
 def getDockerStats(containID):
     return cli.stats(containID, stream=False)
